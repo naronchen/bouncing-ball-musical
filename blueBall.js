72f729bc1781efab492eafbefa4eb91f9a912235
Naron <naron.chen@richmond.edu>
2023-04-12 14:45:55 -0400

    sound better sound

--- blueBall.js
+++ blueBall.js
@@ -7,39 +7,41 @@ class BlueBall extends Ball {
 
         this.core = new Tone.PolySynth({}, Tone.Synth).toMaster();
         this.patch = {
-            frequency: "A4",
+            frequency: "B4",
             detune: 0,
             oscillator: {
-                type: "square"
+                type: "square", // changed oscillator type to "sawtooth"
+                spread: 30, // added spread parameter to create a detuned sound
             },
             filter: {
-                Q: 1,
-                type: "lowpass",
-                rolloff: -24
+                Q: 2, // increased filter Q value for a resonant filter
+                type: "bandpass", // changed filter type to "bandpass" for a more complex frequency response
+                rolloff: -12, // changed filter rolloff to -12dB for a less steep slope
             },
-    
+        
             envelope: {
-                attack: 0.7,
-                decay: 1.0,
-                release: 3.0
+                attack: 0.1, // shortened attack time for a more percussive sound
+                decay: 0.5, // shortened decay time for a more staccato sound
+                release: 2.0, // increased release time for a longer sustain
             },
-    
+        
             filterEnvelope: {
-                attack: 0.7,
-                decay: 1.0,
-                release: 3.0,
-                baseFrequency: 600,
-                octaves: 4
+                attack: 0.2, // increased filter envelope attack time for a more pronounced filter sweep
+                decay: 0.8, // increased filter envelope decay time for a longer filter sweep
+                release: 2.0,
+                baseFrequency: 800, // increased base frequency of the filter sweep
+                octaves: 2, // reduced the number of octaves for a narrower filter sweep
             }
-        }
+        };
         this.core.set(this.patch);
+        
     }
 
     playCollisonSound() {
         // Logic to play collision sound based on information in BlueBall class
         console.log('Playing collision sound for BlueBall');
         // ...
-        this.core.triggerAttackRelease("A4", "4n");
+        this.core.triggerAttackRelease("A3", "8n");
     }
 
     checkCollisionWithBalls(balls) {
