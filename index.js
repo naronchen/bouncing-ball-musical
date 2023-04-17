9a712c3bc40d9950810a2b26e86605f3999915aa
YiliYu2002 <106705601+YiliYu2002@users.noreply.github.com>
2023-04-17 13:40:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
 import Ball from './ball.js';
 import Pool from './pool.js';
-import * as Tone from 'https://cdn.skypack.dev/tone@14.7.77';
 
 
-// console.warn = function () {};
 const pool = new Pool();
 
 
@@ -23,7 +20,7 @@ document.body.appendChild(addBallButton);
 
 
 const colorSelect = document.createElement('select');
-const colors = ['blue', 'orange', 'lightgreen'];
+const colors = ['blue', 'orange', 'yellow'];
 colors.forEach(color => {
   const option = document.createElement('option');
   option.value = color;
@@ -35,7 +32,7 @@ document.body.appendChild(colorSelect);
 
 addBallButton.addEventListener('click', () => {
   const color = colorSelect.value;
-  // console.log("ball color: ", color);
+  console.log("ball color: ", color);
   pool.addBall(color);
 });
 
@@ -65,14 +62,45 @@ container.appendChild(checkbox);
 container.appendChild(label);
 
 checkbox.addEventListener('click', () => {
-  orangeballs.forEach(ball => {
+  pool.balls.forEach(ball => {
     ball.set_vibrato(checkbox.checked)
-  }),
-  blueballs.forEach(ball => {
-    ball.set_vibrato(checkbox.checked)
-  }),
-  yellowballs.forEach(ball => {
-    ball.set_vibrato(checkbox.checked)
-  }
-  );
+  })
+});
+
+// Slider
+const sliderContainer = document.querySelector(".slider-container");
+const sliderTrack = document.querySelector(".slider-track");
+const sliderThumb = document.querySelector(".slider-thumb");
+const sliderValue = document.querySelector("#slider-value");
+
+let isDragging = false;
+
+sliderThumb.addEventListener("mousedown", () => {
+  isDragging = true;
+});
+
+sliderContainer.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+sliderContainer.addEventListener("mousemove", (event) => {
+  if (!isDragging) return;
+  
+  let thumbPosition = event.clientX - sliderContainer.getBoundingClientRect().left;
+  
+  if (thumbPosition < 0) thumbPosition = 0;
+  if (thumbPosition > sliderContainer.offsetWidth) thumbPosition = sliderContainer.offsetWidth;
+  
+  sliderTrack.style.width = `${thumbPosition}px`;
+  sliderThumb.style.left = `${thumbPosition - (sliderThumb.offsetWidth / 2)}px`;
+  
+  // Calculate the value of the slider based on the position of the thumb
+  let sliderValue = Math.round((thumbPosition / sliderContainer.offsetWidth) * 100);
+
+  pool.balls.forEach(ball => {
+    ball.set_filter(sliderValue*200)
+  });
+
+  // Update the text element with the slider value
+  document.querySelector("#slider-value").textContent = sliderValue*200;
 });
